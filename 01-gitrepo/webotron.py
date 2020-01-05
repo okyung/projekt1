827bd72edb8cfafb6ea27c4c4ca5a55753c67071
someone <o.yoon@ymail.com>
2020-01-05 14:28:38 +0100

    changs 5 Jan 2020

---
+++ 01-gitrepo/webotron.py
@@ -0,0 +1,20 @@
+import boto3
+import click
+
+session = boto3.Session(profile_name='pythonAutomation')
+s3 = session.resource('s3')
+
+@click.group()
+def cli():
+    "Webotron deploys websites to AWS"
+    pass
+
+@cli.command('list-buckets')
+def list_buckets():
+    "List all s3 buckets"
+    for bucket in s3.buckets.all():
+        print(bucket)
+
+
+if __name__ == '__main__':
+    list_buckets()
